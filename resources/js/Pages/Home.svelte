e755ad8a6ec0e6ed4efa1cd5931d6286a3d40b36
lasarinii <pedroluislasarini23@gmail.com>
2023-04-25 21:26:07 -0300

    primeiro commit

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,24 @@
+<script>
+    let name = "World";
+
+    let value = 0;
+</script>
+
+<h1>Hello {name}!!!</h1>
+
+<p>{value}</p>
+<button
+    on:click={() => {
+        value--;
+    }}>-</button
+>
+<button
+    on:click={() => {
+        value++;
+    }}>+</button
+><br />
+<button
+    on:click={() => {
+        value = 0;
+    }}>reset</button
+>
